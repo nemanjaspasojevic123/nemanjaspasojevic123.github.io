513376755b1f6f8fc43496fa20827146ce21dd3e
Nemanja Spasojevic <nemanja.spasojevic.memi@gmail.com>
2020-04-03 12:23:12 +0200

    Zavrsen projekat

---
+++ precache-manifest.485d7a9a04324f4233a03bb9986039eb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4411284ad4a856ace2a8afb56c248fd0",
+    "url": "/Zavrsni-projekat/index.html"
+  },
+  {
+    "revision": "f42a68193b173e4f6c70",
+    "url": "/Zavrsni-projekat/static/css/main.aba1b950.chunk.css"
+  },
+  {
+    "revision": "6d53a10d51ce03b35ad7",
+    "url": "/Zavrsni-projekat/static/js/2.6eb8cb99.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Zavrsni-projekat/static/js/2.6eb8cb99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f42a68193b173e4f6c70",
+    "url": "/Zavrsni-projekat/static/js/main.9cbce2a2.chunk.js"
+  },
+  {
+    "revision": "f58ef6c27f641d113978",
+    "url": "/Zavrsni-projekat/static/js/runtime-main.0e119205.js"
+  },
+  {
+    "revision": "70ee47e5d8298711c79782290eceb130",
+    "url": "/Zavrsni-projekat/static/media/1.70ee47e5.jpg"
+  },
+  {
+    "revision": "160bd700aa8a3a6e1d080559c3e404c0",
+    "url": "/Zavrsni-projekat/static/media/login.160bd700.jpg"
+  },
+  {
+    "revision": "2c6e94aaf20d66610132b533ae100324",
+    "url": "/Zavrsni-projekat/static/media/profile.2c6e94aa.png"
+  }
+]);
